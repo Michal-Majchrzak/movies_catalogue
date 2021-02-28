7a4bab609084fd1721018e6b9ea05f501b43d806
Michał <mmajchrzak1@gmail.com>
2021-02-28 10:15:56 +0100

    Layout
    General layout preparation

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def homepage():
+    movies = range(8)
+    return render_template('homepage.html', movies=movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
