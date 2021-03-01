c3f1db34fc5d44ecee5039b8286d75e848b4177e
Michał <mmajchrzak1@gmail.com>
2021-03-01 15:24:09 +0100

    Add TMDB client
    Send requests to TMDB API, display data in homepage.html

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from flask import Flask, render_template
+import tmdb_client
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def homepage():
-    movies = range(8)
+    movies = tmdb_client.get_popular_movies(8)
     return render_template('homepage.html', movies=movies)
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path: str, size: str):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_movie_runtime(movie_id: int):
+        return tmdb_client.get_movie_runtime(movie_id)
+    return {"tmdb_movie_runtime": tmdb_movie_runtime}
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_return_genres(genre_ids: list):
+        return tmdb_client.return_movie_genres(genre_ids)
+    return {"tmdb_return_genres": tmdb_return_genres}
 
 
 if __name__ == '__main__':
---
+++ tmdb_client.py
@@ -0,0 +1,47 @@
+import requests
+
+V4_TOKEN = ""
+
+
+def get_tmdb_response(url: str) -> dict:
+    headers = {'Authorization': f"Bearer {V4_TOKEN}"}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+def _get_movies_genres():
+    url = "https://api.themoviedb.org/3/genre/movie/list"
+    genres = get_tmdb_response(url).get('genres')
+    result = {}
+
+    for genre in genres:
+        result[genre['id']] = genre['name']
+
+    return result
+
+
+MOVIE_GENERES = _get_movies_genres()
+
+
+def return_movie_genres(genre_ids: list) -> str:
+    result = ""
+    for genre_id in genre_ids:
+        result += f"{MOVIE_GENERES.get(genre_id, '')}, "
+    return result
+
+
+def get_movie_runtime(movie_id: int) -> int:
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    result = get_tmdb_response(url)
+    return result.get('runtime', 0)
+
+
+def get_popular_movies(list_len: int = 8) -> dict:
+    url = "https://api.themoviedb.org/3/movie/popular"
+    result = get_tmdb_response(url)
+    return result.get('results')[:list_len]
+
+
+def get_poster_url(api_image_path: str, size: str = 'w342') -> str:
+    secure_base_url = "https://image.tmdb.org/t/p/"
+    return f"{secure_base_url}{size}{api_image_path}"
