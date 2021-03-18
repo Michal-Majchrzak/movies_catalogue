9e08f0705ed55ce673e705bf82ed31881137595a
Michał <mmajchrzak1@gmail.com>
2021-03-18 17:08:18 +0100

    Update main.py
    Add application factory function, also update tests for test_main.py

--- main.py
+++ main.py
@@ -1,55 +1,51 @@
 from flask import Flask, render_template, request, redirect, url_for
 from movies_catalogue import tmdb_client
 
-app = Flask(__name__)
-LIST_TYPES = ['now_playing', 'popular', 'top_rated', 'upcoming']
 
+def create_app():
+    app = Flask(__name__)
+    LIST_TYPES = ['now_playing', 'popular', 'top_rated', 'upcoming']
+    tmdb_client.MOVIE_GENERES = tmdb_client.build_movies_genres_dict_from_tmdb_api()
 
-@app.route('/', methods=['GET'])
-def homepage():
-    passed_list = request.args.get('list_type', '')
+    @app.route('/', methods=['GET'])
+    def homepage():
+        passed_list = request.args.get('list_type', '')
 
-    if passed_list in LIST_TYPES:
-        selected_list = passed_list
-    else:
-        return redirect(url_for('homepage', list_type='popular'))
+        if passed_list in LIST_TYPES:
+            selected_list = passed_list
+        else:
+            return redirect(url_for('homepage', list_type='popular'))
 
-    movies = tmdb_client.get_movies_list(selected_list, 8)
-    return render_template('homepage.html', movies=movies, list_types=LIST_TYPES, selected=selected_list)
+        movies = tmdb_client.get_movies_list(selected_list, 8)
+        return render_template('homepage.html', movies=movies, list_types=LIST_TYPES, selected=selected_list)
 
+    @app.route('/movie/<int:movie_id>', methods=['GET'])
+    def movie_details(movie_id: int):
+        movie = tmdb_client.get_single_movie(movie_id)
+        cast = tmdb_client.get_single_movie_cast(movie_id, 8)
 
-@app.route('/movie/<int:movie_id>', methods=['GET'])
-def movie_details(movie_id: int):
-    movie = tmdb_client.get_single_movie(movie_id)
-    cast = tmdb_client.get_single_movie_cast(movie_id, 8)
+        backdrop = tmdb_client.get_random_movie_backdrop(movie_id)
+        if backdrop:
+            movie['backdrop_path'] = backdrop
 
-    backdrop = tmdb_client.get_random_movie_backdrop(movie_id)
-    if backdrop:
-        movie['backdrop_path'] = backdrop
+        return render_template("movie_details.html", movie=movie, cast=cast)
 
-    return render_template("movie_details.html", movie=movie, cast=cast)
+    @app.context_processor
+    def utility_processor():
+        def tmdb_image_url(path: str, size: str):
+            return tmdb_client.get_poster_url(path, size)
+        return {"tmdb_image_url": tmdb_image_url}
 
+    @app.context_processor
+    def utility_processor():
+        def tmdb_movie_runtime(movie_id: int):
+            return tmdb_client.get_movie_runtime(movie_id)
+        return {"tmdb_movie_runtime": tmdb_movie_runtime}
 
-@app.context_processor
-def utility_processor():
-    def tmdb_image_url(path: str, size: str):
-        return tmdb_client.get_poster_url(path, size)
-    return {"tmdb_image_url": tmdb_image_url}
+    @app.context_processor
+    def utility_processor():
+        def tmdb_return_genres(genre_ids: list):
+            return tmdb_client.return_movie_genres(genre_ids)
+        return {"tmdb_return_genres": tmdb_return_genres}
 
-
-@app.context_processor
-def utility_processor():
-    def tmdb_movie_runtime(movie_id: int):
-        return tmdb_client.get_movie_runtime(movie_id)
-    return {"tmdb_movie_runtime": tmdb_movie_runtime}
-
-
-@app.context_processor
-def utility_processor():
-    def tmdb_return_genres(genre_ids: list):
-        return tmdb_client.return_movie_genres(genre_ids)
-    return {"tmdb_return_genres": tmdb_return_genres}
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return app
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from movies_catalogue.main import app
+from movies_catalogue.main import create_app
 from unittest.mock import Mock
 import pytest
 
@@ -15,6 +15,10 @@ def test_homepage(monkeypatch, list_type):
     api_mock = Mock(return_value={'results': []})
     monkeypatch.setattr('movies_catalogue.tmdb_client.get_tmdb_response', api_mock)
 
+    genre_builder_function_mock = Mock(return_value={})
+    monkeypatch.setattr('movies_catalogue.tmdb_client.build_movies_genres_dict_from_tmdb_api', genre_builder_function_mock)
+
+    app = create_app()
     with app.test_client() as client:
         response = client.get(f'/?list_type={list_type}')
         assert response.status_code == 200
